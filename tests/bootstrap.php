b55bd7a61f9935a9ca9f4b235654cf14d591c08f
Георгий Либиков <xarmsandbox@mail.ru>
2025-11-13 01:49:42 +0300

    app

---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+    // Переопределить переменные из файла .env.test
+    (new Dotenv())->overload(path: dirname(__DIR__) . '/.env.test');
+}
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
